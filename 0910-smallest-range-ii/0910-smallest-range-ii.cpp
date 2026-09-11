c2e92c87a0e0e539272bb1dd03661a52b875374d
PRINCE CHAUBEY <chaubeybablu62@gmail.com>
2026-09-12 00:41:36 +0530

    Time: 3 ms (54.59%), Space: 19.2 MB (98.22%) - LeetHub

---
+++ 0910-smallest-range-ii/0910-smallest-range-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int smallestRangeII(vector<int>& nums, int k) {
+          int n = nums.size();
+          if(n==1) return 0;
+
+          sort(nums.begin(), nums.end());
+          int res = (nums[n-1] - nums[0]); // Sort karne ke baad highest aur lowest ka diff
+          for(int i=0;i<n-1;i++){
+          int maxi = max(nums[n-1] - k, nums[i] + k);
+          int mini = min(nums[0] + k, nums[i+1] - k);
+            res = min(res, maxi - mini);
+          }
+          return res;
+    }
+};
