c5026d71748d86ee1a19a8a84804b4c9bc796b18
PRINCE CHAUBEY <chaubeybablu62@gmail.com>
2026-09-14 23:43:32 +0530

    Time: 0 ms (100%), Space: 11.1 MB (98.71%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,29 @@
+// same as question number 62 
+// only check obstacle present or not 
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& arr) {
+        int m = arr.size(), n = arr[0].size();
+        int dp[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    if (i != 0 && j != 0) {
+                        dp[i][j] = dp[i - 1][j] +
+                                   dp[i][j - 1]; // ak row uper same colum and
+                                                 // same row ak previous colum
+                    } else if (i == 0 && j != 0) {
+                        dp[i][j] = dp[i][j - 1]; // previous culum
+                    } else if (j == 0 && i != 0) {
+                        dp[i][j] = dp[i - 1][j]; // previous row
+                    }
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
